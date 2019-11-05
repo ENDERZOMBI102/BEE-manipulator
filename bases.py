1e004d6f18cba629b20a9905604553e41f1be4c4
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2019-11-05 20:14:30 +0100

    added updateui.py

--- bases.py
+++ bases.py
@@ -15,16 +15,6 @@ class web:
 	  except:
 		  conn.close()
 		  return False
-		  
-		  
-	def checkUpdates():
-		if(web.isonline==False):
-			return False
-		ov=get('https://api.github.com/repos/ENDERZOMBI102/BEE-manipulator/releases/latest').json()
-		if(not config.load('appVersion')>=ov['tag_name']):
-			return True
-		else:
-			return False
 
 	def installUpdates():
 		print("")
