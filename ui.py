597e9ba64728d83139ea1f5565d47caccb284d0f
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-01-17 09:41:21 +0000

    things

--- ui.py
+++ ui.py
@@ -4,11 +4,17 @@ from tkinter.ttk import Notebook
 from config import *
 
 class root(tk.Tk):
-	def __init__(self):
-		super().__init__()
-		self.title("BEE Manipulator v"+str(config.load("appVersion")))
-		self.geometry("600x500")
-		#the main container
+    def __init__(self):
+        super().__init__()
+        self.title("BEE Manipulator v"+str(config.load("appVersion")))
+        self.geometry("600x500")
+        #the main container
+        self.mainFrame = tk.Frame(self)
+        #
+        #
+        #
+        #
+        self.mainFrame.pack()
 		
 if __name__=="__main__":
 	root=root()
