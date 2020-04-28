aa99b22bd88431e01da7f40e51d89f955906bd77
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-04-28 17:11:07 +0200

    "finished" discord rich presence (for now)

--- richPresence.py
+++ richPresence.py
@@ -1,37 +1,40 @@
-from pypresence import Presence, Activity
-from config import reconfig
+import pypresence
+import config
+from utilities import startTime
+from srctools.logger import get_logger
 from time import sleep
 
-class richPresence():
-      r"""
-            the main class for the discord rich presence integration.
-            create an object to initialize the class
-      """
+presence = None
+logger = get_logger()
+
+class richPresence:
+      global logger
+      rpc: pypresence.Presence
       def __init__(self):
-            self.RPC = Presence(reconfig.discordToken())  # Initialize the Presence client
-            self.RPC.connect() # connect the client
-            self.RPC.update(state="Starting BEE Manipulator!")
-            """
-                  this will let discord know that the app is still running
-            """
-            while(True):
-                  sleep(15)
+            logger.info('loading discord integration..')
+            self.rpc = pypresence.Presence(config.discordToken)
+            logger.debug('rich presence integration loaded!')
+            self._connect()
+            logger.info('connected to discord!')
+            logger.debug('updating discord rich presence')
+            self.rpc.update(start=startTime)
 
-      def update(self, data = "init"):
-            r"""
-                  can be: init, brows, idle, settings, BEE
-            """
-            if data == "init":
-                  self.RPC.update(state="Starting BEE Manipulator!")
-            elif data == "brows":
-                  self.RPC.update(state="Browsing BEEmod packages.")
-            elif data == "idle":
-                  self.RPC.update(state="Idle")
-            elif data == "settings":
-                  self.RPC.update(state="In the settings tab")
-            elif data.upper() == "BEE":
-                  self.RPC.update(state="Using BEE2.4")
+      def _connect(self) -> None:
+            try:
+                  logger.debug(f'making discord connection attempt...')
+                  self.rpc.connect()
+            except:
+                  logger.warning("can't connect to discord! making new attempt in 20s")
+                  sleep(20)
+                  self._connect()
 
+def init() -> None:
+      global presence
+      presence = richPresence()
 
+def getRpc() -> pypresence.Presence:
+      return presence.rpc
+
+      
 if __name__ == "__main__":
-      obj = richPresence()
+      pass
