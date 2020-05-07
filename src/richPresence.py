9a3a3b3f92fb434f885a33d286a9d6743b64f617
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-05-07 14:35:20 +0200

    not working

--- src/richPresence.py
+++ src/richPresence.py
@@ -7,60 +7,59 @@ import requests
 from srctools.logger import get_logger
 
 logger = get_logger()
-richPresence: pypresence.Presence = None
-lastUpdate: dict = {"start": int(time.time()), "state": None, "details": None, "palette": None}
-auth = None
-client: pypresence.Client = None
-
-async def rich():
-      #rpc = pypresence.Presence(config.discordToken)
-      client = pypresence.Client(config.discordToken)
-      while True:
-            try:
-                  client.start()
-                  break
-            except:
-                  logger.warning(r"can't connect to discord, retry in 20s")
-                  await asyncio.sleep(20)
-      logger.info('successfully connected to discord!')
-      logger.info('waiting for authorization...')
-      if config.load('OAuth2') == None:
-            while True:
-                  try:
-                        logger.debug('sending RPC command "authorize"')
-                        auth = client.authorize(config.discordToken, ['rpc'])
-                        break
-                  except:
-                        asyncio.sleep(20)
-            logger.info('authorization recived!')
-            logger.debug('sending POST authorization code to discord servers')
-            
-            client.authenticate(auth['data']['code'])
-      update({})
-
+rpc: pypresence.Presence = None
+lastUpdate: dict = {
+						'start': int(time.time()),
+						'state': None,
+						'details': None,
+						'largeImage': None,
+						'largeText': None,
+						'smallImage': None,
+						'smallText': None
+				   }
 
 async def loadPalette():
-      update()
+	update({})
 
-def update(updateData: dict):
-      for key, data in updateData:
-            lastUpdate[key] = data
-      client.set_activity(state=lastUpdate['state'],
-                          details=lastUpdate['details'],
-                          start=updateData['start'])
+async def update(updateData: dict):
+	for key, data in updateData:
+		lastUpdate[key] = data
+	logger.debug(
+f'''
+updating discord activity\n
+- new state: {lastUpdate['state']}\n
+- new details: {lastUpdate['details']}
+''')# a giant log :P
+	rpc.update(
+				state = lastUpdate['state'],
+				details = lastUpdate['details'],
+				start = lastUpdate['start'],
+				large_image = lastUpdate['largeImage'],
+				large_text = lastUpdate['largeText'],
+				small_image = lastUpdate['smallImage'],
+				small_text = lastUpdate['smallText']
+			  )
 
-def init():
-      asyncio.run(rich())
+def simpleInit():
+	logger.debug('starting "asyncio" thread for "rich presence"')
+	asyncio.run(simpleRich())
 
-#def startEventListener():
-#      app = servepy.App()
-#      app.all('BMEVTHANDLER', eventListener)
-#
-#def eventListener(req: servepy.Request, res: servepy.Response):
-#      logger.info(f'recived data on event listener: {req.body}')
-#
-
-
+async def simpleRich():  
+	rpc = pypresence.Presence(config.discordToken)
+	while True:
+		try:
+			logger.info('attempting to connect to discord..')
+			await rpc.connect()
+			break
+		except:
+			logger.warning(r"can't connect to discord, retry in 20s")
+			await asyncio.sleep(20)
+	logger.info('successfully connected to discord!')
+	if config.devMode():
+		update({'details': 'Developing new features!', 'state': 'why is UI difficult?'})
+	else: update({'details': 'idle'})
 
 if __name__ == "__main__":
-      init()
+	simpleInit()
+	time.sleep(30)
+	update({"state": "hi world!", "details": "making new features"})
