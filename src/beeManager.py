176562de0208dae0710262f18dad693c51c74d37
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-05-20 21:33:40 +0200

    started working on bee install

--- src/beeManager.py
+++ src/beeManager.py
@@ -1,47 +1,52 @@
-from requests import *
-from subprocess import *
-from zipfile import *
-from threading import Thread
+import configparser
+import io
+import os
+from pathlib import Path
 from sys import platform as os
-from os import getenv
-from config import *
-from utilities import boolcmp
-from io import BytesIO
+from zipfile import *
 
-def checkBeeUpdates():#return true if an update is available, false if there isn't or the pc is offline
-    if not load("beeUpdateUrl") == "None":
-        return True 
-    try:# get the latest metadata
+from requests import *
+
+import config
+from utilities import boolcmp
+
+
+def checkBeeUpdates():  # return true if an update is available, false if there isn't or the pc is offline
+    if not config.load("beeUpdateUrl") == "None":
+        return True
+    try:  # get the latest metadata
         data = get("https://api.github.com/repos/BEEmod/BEE2.4/releases/latest")
-    except:# if an error is raised, the pc is offline, so we return False
+    except:  # if an error is raised, the pc is offline, so we return False
         return False
-    onlineVersion = []# create empty list
+    onlineVersion = []  # create empty list
     # iterate in every character in the version string, if there's a invalid character, we remove it
     for i in data["tag_name"]:
-        if i in ['0','1','2','3','4','5','6','7','8','9','.']:
-            onlineVersion.append(i)# add the char to the final list
-    onlineVersion = "".join(onlineVersion)# the list is converted to a string
-    if int(onlineVersion) > version():# is the online version more updated? if not, return False
-        if boolcmp(data["draft"]):# is the online version a draft? if yes return False
-            save(None, "beeUpdateUrl")
+        if i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']:
+            onlineVersion.append(i)  # add the char to the final list
+    onlineVersion = "".join(onlineVersion)  # the list is converted to a string
+    if int(onlineVersion) > config.version():  # is the online version more updated? if not, return False
+        if boolcmp(data["draft"]):  # is the online version a draft? if yes return False
+            config.save(None, "beeUpdateUrl")
             return False
-        # check if a prerelease is avaiable, return false if it is
+        # check if a prerelease is available, return false if it is
         elif boolcmp(data["prerelease"]):
-            save(None, "beeUpdateUrl")
+            config.save(None, "beeUpdateUrl")
             return False
-        else:# else return true and save the download url
-            save(getUrl(data), "beeUpdateUrl")
+        else:  # else return true and save the download url
+            config.save(getUrl(data), "beeUpdateUrl")
             return True
 
+
 def getUrl(data):
-    # cicle in the assets
+    # cycle in the assets
     for asset in data["assets"]:
         # get the correct url for the OS we're on
         if "mac" in asset["name"] and "darwin" in os:
             return asset["browser_download_url"]
         elif "win" in asset["name"] and "win" in os:
             return asset["browser_download_url"]
-    raise Exception("how this happened?, you're on linux?")        
+    raise Exception("how this happened?, you're on linux?")
+
 
 def updateBee():
     r"""
@@ -51,40 +56,31 @@ def updateBee():
     """
     # get the json data
     try:
-        r = get(load("beeUpdateUrl"))#download BEE
+        r = get(config.load("beeUpdateUrl"))  # download BEE
     except Exception as e:
-        raise downloadError("failed to complete the download.\n"+e)
-    zipdata = ZipFile(BytesIO(r.content)) # load and convert the data to a bytes stream
-    zipdata.extractall("BEE2")# extract BEE
+        raise downloadError("failed to complete the download.\n" + e)
+    zipdata = ZipFile(io.BytesIO(r.content))  # load and convert the data to a bytes stream
+    zipdata.extractall("BEE2")  # extract BEE
     try:
         data = get('https://api.github.com/repos/BEEmod/BEE2-items/releases/latest').json()
     except Exception as e:
-        raise downloadError("failed to complete the donwload.\n"+e)
+        raise downloadError("failed to complete the download.\n" + e)
     d_url = data['assets'][0]['browser_download_url']
-    data = get(d_url)	
-    zipdata = ZipFile(BytesIO(data.content))
-    zipdata.extractall(load("beePath"))
+    data = get(d_url)
+    zipdata = ZipFile(io.BytesIO(data.content))
+    zipdata.extractall(config.load("beePath"))
 
-async def startBee():
-    r"""
-    Use this to start BEE2
-    raise an exception if we're not on windows
-    """
-    if os == "win32":
-        await run(load('BEEpath')+"/BEE2.exe")
-    else:
-        raise Exception("you should use this with windows")
 
 def verifyGameCache():
     # try to delete the bee2 folder ine p2 root dir
     return
     try:
-        os.rmdir(portalDir() + "/bee2")
+        os.rmdir(config.portalDir() + "/bee2")
     except:
         pass
     # try to remove the bee2 dir inside the bin folder
     try:
-        os.rmdir(portalDir() + "/bin/bee2")
+        os.rmdir(config.portalDir() + "/bin/bee2")
     except:
         pass
     # try to remove the bee2 compilers
@@ -100,15 +96,60 @@ class configManager:
     this definition contains some userful commands to
     manipulate the config files
     """
-    def addGame(path = ""):
-        import configparser
+
+    @staticmethod
+    def addGame(path='', name='Portal 2', overwrite=False):
+        # the games.cfg file path (its where the games data is stored)
+        gamescfgPath = os.getenv('APPDATA').replace("\\", "/") + "/BEEMOD2/config/games.cfg"
+        # the cfg is a ini formatted file so import a std lib that can handle them
         data = configparser.ConfigParser()
-        data['Portal 2'] = {'steamid': '620', 'dir': path}
-        with open(getenv('APPDATA').replace("\\", "/") + "/BEEMOD2/config/games.cfg", "w") as file:
-            data.write(file)
+        # open the cfg for reading
+        file = open(gamescfgPath, "r")
+        # read the cfg
+        data.read_file(file)
+        # close the cfg
+        file.close()
+        # if the section that we want to write already exist raise an exception apart when overwrite is True
+        if data.has_section(name) and overwrite:
+            # apply the new game
+            data[name] = {'steamid': '620', 'dir': path}
+        elif not data.has_section(name):
+            # apply the new game
+            data[name] = {'steamid': '620', 'dir': path}
+        else:
+            raise GameAlreadyExistError(f'key name {name} already exist! use another name or the overwrite!')
+        # reopen the cfg for writing
+        file = open(gamescfgPath, 'w')
+        # write it
+        data.write(file)
+        # close it
+        file.close()
+
+    @staticmethod
+    def createAndAddGame(name='Portal 2', loc='', steamid=0):
+        # the games.cfg file path (its where the games data is stored)
+        gamescfgPath = os.getenv('APPDATA').replace("\\", "/") + "/BEEMOD2/config/games.cfg"
+        if Path(gamescfgPath).exists():
+            raise Exception('can\' create the file, they already exist!')
+        # create the necessary folders
+        Path(gamescfgPath + '/../..').resolve().mkdir()
+        Path(gamescfgPath + '/..').resolve().mkdir()
+        data = configparser.ConfigParser()
+        # create and open the file for writing
+        file = open(gamescfgPath, 'x')
+        # the data
+        data[name] = {'steamid': '620', 'dir': loc}
+        # write it
+        data.write(file)
+        # close it
+        file.close()
 
 
-class downloadError(Exception):
-    pass
+class downloadError(Exception): pass
+
+
+class GameAlreadyExistError(Exception): pass
+
+
 if __name__ == "__main__":
-    configManager.addGame("c:/hellothere")
+    configManager.addGame("c:/hellothere", 'test')
