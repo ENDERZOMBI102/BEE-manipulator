f2a180ce979634be4be04919a6806c9d3b5ae8d5
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-06-18 17:16:38 +0200

    fix build process

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
 @echo off
-cls
-del dist /Q
-if %~1 == "release" (
-	del BEEManipulator.7z
-    pyinstaller src/BEEManipulator_release.spec --noconfirm
-    C:\"Program Files"\7-Zip\7z a -r BEEManipulator ./dist/"BEE Manipulator"/*
-) else (
-    pyinstaller src/BEEManipulator_debug.spec --noconfirm
-)
+%~0
+exit
