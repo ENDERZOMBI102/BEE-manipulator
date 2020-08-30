34fd7e44fdc9b324695215c819933c1c9a009dc9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-08-30 20:12:37 +0200

    implemented ipc server/connection classes TODO: to test

--- src/ipc.py
+++ src/ipc.py
@@ -1,54 +1,85 @@
-import asyncio
 import socket
-from typing import Callable, List
+import threading
+import weakref
+from typing import Callable, List, Dict
 
 from srctools.logger import get_logger
 
 logger = get_logger()
+# TODO: test all this
 
+class Connection:
 
-def connection(self, conn: socket.socket, addr: str, master: object):
+	conn: socket.socket
+	port: int
 	closed: bool = False
-	logger.debug(f'opened connection with {addr}.')
-	logger.debug('waiting for data..')
-	while not closed:
-		length = self.conn.recv(30).decode()
-		logger.debug(f'receiving data (length: {length}).')
-		data = self.conn.recv(length).decode()
-		logger.debug(f'received data from {addr}.')
-		if data == '!DiSCONNECT':
-			conn.close()
-			closed = True
-		else:
-			for handle in master.handles:
-				handle(data)
+	thread: threading.Thread
+
+	def __init__(self, conn: socket.socket, server: ipcServer):
+		self.conn = conn
+		self.server = server
+		self.thread = threading.Thread( target=self.rcv, daemon=True )
+		self.thread.start()
+
+	def rcv(self):
+		while not self.closed:
+			raw_size = self.conn.recv(64)
+			size = int( raw_size.decode() )
+			data = self.conn.recv(size)
+
+			if data == '!SHUTDOWN_CONNECTION':
+				self.closed = True
+				continue
+			parts = data.split('/')
+			if parts[2] in self.server.handlers.keys():
+				for hdlr in self.server.handlers[parts[2]]:
+					hdlr(data)
+		# close
+		self.conn.close()
+		self.thread.join()
+
+	def shutdown(self):
+		self.closed = True
+		self.conn.close()
+		self.thread.join()
 
 
 class ipcServer:
 	"""
 	this class represents an IPC socket server.
 	"""
-	handlers: List[Callable]
+	handlers: Dict[str, List[Callable] ]
 	listening: bool
 	port: int = 20306
+	connections: List[ weakref.ref ]
 
 	def __init__(self, port: int = None):
 		if port:
 			self.port = port
 		logger.info(f'starting IPC server on port {self.port}')
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.bind(('localhost', self.port))
-		asyncio.run(self.run())
+		self.socket.bind( ('localhost', self.port) )
+		logger.debug('starting listener')
+		threading.Thread( target=self.serve_forever )
 
-	def listen(self):
-		self.socket.listen()
-		self.listening = True
-
-	async def run(self):
+	def serve_forever(self):
 		"""
 		this function starts the IPC server
 		"""
-		self.listening()
 		while self.listening:
 			conn, addr = self.socket.accept()
-			connection(conn, addr, self)
+			self.connections.append( weakref.ref( Connection(conn, self) ) )
+
+	def registerHandler(self, handler: Callable, proc: str):
+		if proc in self.handlers.keys():
+			self.handlers[proc].append(handler)
+		else:
+			self.handlers[proc] = []
+			self.handlers.append(handler)
+
+
+
+
+
+server = ipcServer()
+
