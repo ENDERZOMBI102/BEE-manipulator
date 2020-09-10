03c6bb9c152675c5fe56162ab1aca14d94f0afc0
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-09-10 18:15:25 +0200

    created themeManager.py

---
+++ src/themeManager.py
@@ -0,0 +1,18 @@
+import wx
+
+
+class theme:
+	background: wx.Colour
+	foreground: wx.Colour
+	button: wx.Colour
+
+
+class manager:
+
+	def __init__(self):
+		pass
+
+	def getThemes(self):
+		pass
+
+	
