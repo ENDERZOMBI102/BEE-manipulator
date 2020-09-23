4769777e49eaa2c2b79d301d64bd9cb79cadc095
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-09-24 00:23:25 +0200

    Update devtool.py

--- devtool.py
+++ devtool.py
@@ -33,9 +33,9 @@ if '--start' in argv:
     system('%userprofile%/.virtualenvs/BEE-manipulator/scripts/activate && py BEEManipulator.py')
 
 if ('--build' in argv) and ('--release' in argv):
-    system('%userprofile%/.virtualenvs/BEE-manipulator/scripts/activate')
-    system('pyinstaller src/BEEManipulator_release.spec --noconfirm')
-    system(f'pyinstaller src/urlhandler/urlhandler.spec --noconfirm --upx-dir="{upxpath}" --distpath="{distpath}"')
+    system('%userprofile%/.virtualenvs/BEE-manipulator/scripts/activate && '+
+    'pyinstaller src/BEEManipulator_release.spec --noconfirm && '+
+    f'pyinstaller src/urlhandler/urlhandler.spec --noconfirm --upx-dir="{upxpath}" --distpath="{distpath}"')
 
 if ('--build' in argv) and ('--debug' in argv):
     system('%userprofile%/.virtualenvs/BEE-manipulator/scripts/activate &&')
