22b3a98a96fe75b7400007b5f5f33a4c6b8c26cf
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-02 23:51:51 +0200

    added PDK (Plugin Development Kit)

---
+++ docs/BM_PDK/pluginSystem.pyi
@@ -0,0 +1,28 @@
+from abc import ABCMeta, abstractmethod
+from logging import Logger
+
+class Plugin:
+
+	def __init__(self, pluginid: str, name: str, version: str = None):
+		"""
+		this decorator is used to decorate a class that contains a plugin for BEE Manipulator
+		"""
+		pass
+
+	def __call__(self, cls):
+		pass
+
+
+def getLogger() -> Logger:
+	pass
+
+
+class BasePlugin(metaclass=ABCMeta):
+
+	@abstractmethod
+	async def load(self):
+		pass
+
+	@abstractmethod
+	async def unload(self):
+		pass
