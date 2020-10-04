cccf65dd18298414a2c2bdc79ca23f2fc14a71f6
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-04 20:00:29 +0200

    improved plugin (re)(un)loading now using class name as plugin id added Event class

--- src/pluginSystem.py
+++ src/pluginSystem.py
@@ -2,48 +2,57 @@ import asyncio
 import importlib.util
 import traceback
 from abc import ABCMeta, abstractmethod
+from enum import Enum
 from pathlib import Path
 from types import ModuleType
-from typing import Dict, Coroutine, List, Callable
+from typing import Dict, Callable
+
+import wx
 
 import config
 from srctools.logger import get_logger
 
 logger = get_logger()
-_eventHandlers: Dict[str, List[Coroutine]] = {}
+
+
+class Events(Enum):
+
+	RegisterMenus = 'RegisterMenusEvent'
+	LogWindowCreated = 'LogWindow'
 
 
 class Plugin:
 	"""
 	this decorator is used to create a plugin for BEE Manipulator
 	"""
-	def __init__(self, pluginid: str, name: str, version: str = None ):
-		self.pluginid = pluginid
+	def __init__(self, name: str, version: str = None ):
 		self.name = name
 		self.version = version
 
 	def __call__(self, cls):
 		# make a subclass to check for methods
-		class wrap(cls):
+		class WrappedPlugin(cls):
 			__state__ = 'unloaded'
 
-			def getPath(self):
-				return self.__class__.__base__.__module__
+			def getPath(self) -> Path:
+				return Path( systemObj.modules[ self.__class__.__base__.__module__ ].__file__ ).resolve()
+		self.pluginid = WrappedPlugin.__base__.__name__
 		# checks if the plugin has the NEEDED methods
+		logger.debug(f'instantiated plugin {self.pluginid} from {WrappedPlugin.__base__.__module__}')
 		# load
-		if not asyncio.iscoroutinefunction(getattr(wrap, 'load', Callable)):
+		if not asyncio.iscoroutinefunction(getattr(WrappedPlugin, 'load', Callable)):
 			raise PluginNotValid('missing required coroutine "load"!')
 		# unload
-		if not asyncio.iscoroutinefunction(getattr(wrap, 'unload', Callable)):
+		if not asyncio.iscoroutinefunction(getattr(WrappedPlugin, 'unload', Callable)):
 			raise PluginNotValid('missing required coroutine "unload"!')
 		# checks the optional methods
 		# reload
-		if not asyncio.iscoroutinefunction(getattr(wrap, 'reload', placeholder)):
+		if not asyncio.iscoroutinefunction(getattr(WrappedPlugin, 'reload', placeholder)):
 			raise PluginNotValid('the "reload" method should be a coroutine')
 		if self.pluginid in systemObj.plugins.keys():
 			if not systemObj.isReloading:
 				logger.error(f'Duplicate plugin found! id: {self.pluginid}, duplicate name: {self.name}, it will replace the other plugin')
-		systemObj.plugins[self.pluginid] = wrap()
+		systemObj.plugins[self.pluginid] = WrappedPlugin()
 		return wrap
 
 
@@ -81,12 +90,17 @@ class BasePlugin(metaclass=ABCMeta):
 		"""
 		pass
 
+	def getPath(self) -> Path:
+		return Path(systemObj.modules[self.__class__.__base__.__module__].__file__).resolve()
+
+	__state__: str
+
 
 class system:
 
 	isReloading: bool = False
-	plugins: Dict[str, object] = {}
-	modules: List[ModuleType] = []
+	plugins: Dict[str, BasePlugin] = {}
+	modules: Dict[str, ModuleType] = {}
 
 	def __init__(self):
 		pass
@@ -115,7 +129,7 @@ class system:
 			name = plg.name.replace('.py', '')
 			spec = importlib.util.spec_from_file_location(name, plg)
 			module = importlib.util.module_from_spec(spec)
-			self.modules.append(module)
+			self.modules[module.__name__] = module
 			try:
 				spec.loader.exec_module(module)
 			except PluginNotValid as e:
@@ -133,11 +147,19 @@ class system:
 		if identifier is not None:
 			if self.plugins[identifier].__state__ == 'loaded':
 				raise Exception('trying to load an already loaded plugin!')
-			await self.plugins[identifier].load()
+			try:
+				await self.plugins[identifier].load()
+			except Exception as e:
+				logger.error(f'caught exception while loading plugin "{identifier}"')
+				logger.error( ''.join( traceback.format_exception( type(e), e, e.__traceback__ ) ) )
 			self.plugins[identifier].__state__ = 'loaded'
 			return
 		for plg in self.plugins.values():
-			await plg.load()
+			try:
+				await plg.load()
+			except Exception as e:
+				logger.error(f'caught exception while loading plugin "{identifier}"')
+				logger.error( ''.join( traceback.format_exception( type(e), e, e.__traceback__ ) ) )
 			plg.__state__ = 'loaded'
 
 	async def unload(self, identifier: str = None):
@@ -149,12 +171,22 @@ class system:
 		if identifier is not None:
 			if self.plugins[identifier].__state__ == 'unloaded':
 				raise Exception('trying to unload an already unloaded plugin!')
-			await self.plugins[identifier].unload()
+			try:
+				await self.plugins[identifier].unload()
+			except Exception as e:
+				logger.error(f'caught exception while unloading plugin "{identifier}"')
+				logger.error( ''.join( traceback.format_exception( type(e), e, e.__traceback__ ) ) )
 			self.plugins[identifier].__state__ = 'unloaded'
 			return
 		for plg in self.plugins.values():
-			await plg.unload()
+			try:
+				await plg.unload()
+			except Exception as e:
+				logger.error(f'caught exception while unloading plugin "{plg.__class__.__name__}"')
+				logger.error( ''.join( traceback.format_exception( type(e), e, e.__traceback__ ) ) )
 			plg.__state__ = 'unloaded'
+		# redraw the menu bar in case a plugin added something
+		wx.GetTopLevelWindows()[0].menuBar.Refresh()
 
 	async def reload(self, identifier: str):
 		"""
@@ -175,7 +207,7 @@ class system:
 		:return: nothing
 		"""
 		# cicle in the plugins
-		print( f'plugins found: {len( self.plugins)}' )
+		logger.info( f'plugins found: {len( self.plugins)}' )
 		for i in range( len( self.plugins) ):
 			# we're reloading, set isReloading to True
 			self.isReloading = True
@@ -184,19 +216,36 @@ class system:
 			for key in self.plugins.keys():
 				if f == i:
 					pluginid = key
+					break
 				f += 1
-			module = self.modules[i]
+			module = self.modules[ self.plugins[pluginid].__module__ ]
 			print('stop!')
 			# wait for the plugin to unload
-			await self.plugins[pluginid].unload()
+			try:
+				await self.plugins[pluginid].unload()
+			except Exception as e:
+				logger.error(f'caught exception while unloading plugin "{pluginid}"')
+				logger.error( ''.join( traceback.format_exception( type(e), e, e.__traceback__ ) ) )
 			# get the plugin's module spec
 			spec = importlib.util.spec_from_file_location( name=module.__name__, location=module.__file__ )
 			# get the plugin's module
 			module = importlib.util.module_from_spec(spec)
-			# execute the plugin's module
-			spec.loader.exec_module(module)
+			# try to execute the plugin's module
+			try:
+				spec.loader.exec_module(module)
+			except PluginNotValid as e:
+				logger.error(f"can't load a plugin! error:\n{e}")
+			except Exception as e:
+				error = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
+				logger.error(f"can't load plugin! error: {error}")
+			# reassign the module in the dict
+			self.modules[module.__name__] = module
 			# wait for the plugin to load
-			await self.plugins[pluginid].load()
+			try:
+				await self.plugins[pluginid].load()
+			except Exception as e:
+				logger.error(f'caught exception while loading plugin "{pluginid}"')
+				logger.error( ''.join( traceback.format_exception( type(e), e, e.__traceback__ ) ) )
 			# change the plugin's state to loaded
 			self.plugins[pluginid].__state__ = 'loaded'
 			# no mo reloading
