28cea5c6d78f2f0d0aeb68f7c3700626d6e4f467
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-20 13:11:25 +0200

    fixed missing icon fixed version info not working

--- src/aboutWindow.py
+++ src/aboutWindow.py
@@ -6,44 +6,45 @@ import wx.richtext
 from markdown2 import markdown
 
 import config
+import utilities
 from srctools.logger import get_logger
 
 
 def init(master):
-    aboutWindow(master)
+	aboutWindow(master)
 
 
 class aboutWindow(wx.Frame):
 
-    logger = get_logger()
+	logger = get_logger()
 
-    def __init__(self, master):
-        super().__init__(master, title='About BEE Manipulator', style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
-        # self.SetSize( wx.Size(300, 260) )
-        self.SetIcon( wx.Icon(f'{config.assetsPath}icon.ico') )
-        self.box = wx.html.HtmlWindow( self )
-        try:
-            self.logger.debug('trying to open about.html..')
-            with open(f'{config.assetsPath}/about.html', 'r') as file:
-                self.logger.debug('opened about.html!')
-                data = file.read().replace(r'{0}', config.version())
-        except FileNotFoundError:
-            self.logger.warning('failed to load about.html! falling back to about.md')
-            self.logger.debug('opening about.md..')
-            # set the page to the converted markdown text
-            with open(f'{config.assetsPath}/about.md', 'r') as file:
-                self.logger.debug('converting markdown to html..')
-                data = markdown(file.read())
-            with open(f'{config.assetsPath}/about.html', 'w') as file:
-                file.write(data)
-            data = data.replace(r'{0}', config.version())
-        self.logger.debug('loaded html data! displaying..')
-        self.box.SetPage(data)
-        self.CenterOnParent()
-        self.Raise()
-        self.Show()
-        self.box.OnLinkClicked = self.linkHandler
+	def __init__(self, master):
+		super().__init__(master, title='About BEE Manipulator', style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+		# self.SetSize( wx.Size(300, 260) )
+		self.SetIcon( utilities.icon )
+		self.box = wx.html.HtmlWindow( self )
+		try:
+			self.logger.debug('trying to open about.html..')
+			with open(f'{config.assetsPath}/about.html', 'r') as file:
+				self.logger.debug('opened about.html!')
+				data = file.read().replace( r'{0}', config.version.__str__() )
+		except FileNotFoundError:
+			self.logger.warning('failed to load about.html! falling back to about.md')
+			self.logger.debug('opening about.md..')
+			# set the page to the converted markdown text
+			with open(f'{config.assetsPath}/about.md', 'r') as file:
+				self.logger.debug('converting markdown to html..')
+				data = markdown(file.read())
+			with open(f'{config.assetsPath}/about.html', 'w') as file:
+				file.write(data)
+			data = data.replace(r'{0}', config.version())
+		self.logger.debug('loaded html data! displaying..')
+		self.box.SetPage(data)
+		self.CenterOnParent()
+		self.Raise()
+		self.Show()
+		self.box.OnLinkClicked = self.linkHandler
 
-    @staticmethod
-    def linkHandler(link):
-        wb.open(link.GetHref())
+	@staticmethod
+	def linkHandler(link):
+		wb.open(link.GetHref())
