2b776e9ba246ca8f7356abf6fa2b575438560471
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-11-03 15:35:36 +0100

    refactor: removed even more code

--- src/browser.py
+++ src/browser.py
@@ -1,6 +1,5 @@
 import wx
 
-from database import PDatabase
 from srctools.logger import get_logger
 
 logger = get_logger()
@@ -10,16 +9,10 @@ class Browser(wx.ScrolledWindow):
 	"""
 		the package browser, this will display all the available packages
 	"""
-	sizer: wx.GridBagSizer
 
 	def __init__(self, master: wx.Window):
 		super().__init__(
 			parent=master,
 			size=master.GetSize()
 		)
-		# wx.CallAfter( PDatabase.load() )
 
-
-
-	def reload(self):
-		self.database = PDatabase()
