c7bf5d62ff6d2a27def65590ef3529d6889a558d
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-11-13 13:31:36 +0100

    now the window obj is cached, after the first opening, its faster

--- src/aboutWindow.py
+++ src/aboutWindow.py
@@ -6,20 +6,27 @@ import wx.richtext
 from markdown2 import markdown
 
 import config
+import uiWX
 import utilities
 from srctools.logger import get_logger
 
 
-def init(master):
-	aboutWindow(master)
+def init():
+	if aboutWindow.instance is None:
+		aboutWindow()
+	else:
+		aboutWindow.instance.CenterOnParent()
+		aboutWindow.instance.Raise()
+		aboutWindow.instance.Show(True)
 
 
 class aboutWindow(wx.Frame):
 
 	logger = get_logger()
+	instance: 'aboutWindow' = None
 
-	def __init__(self, master):
-		super().__init__(master, title='About BEE Manipulator', style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+	def __init__(self):
+		super().__init__(uiWX.root.instance, title='About BEE Manipulator', style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
 		# self.SetSize( wx.Size(300, 260) )
 		self.SetIcon( utilities.icon )
 		self.box = wx.html.HtmlWindow( self )
@@ -37,14 +44,19 @@ class aboutWindow(wx.Frame):
 				data = markdown(file.read())
 			with open(f'{config.assetsPath}/about.html', 'w') as file:
 				file.write(data)
-			data = data.replace(r'{0}', config.version())
-		self.logger.debug('loaded html data! displaying..')
+			data = data.replace(r'{0}', config.version.__str__() )
 		self.box.SetPage(data)
+		self.box.OnLinkClicked = self.linkHandler
+		self.Bind(wx.EVT_CLOSE, self.OnClose, self)
+		self.logger.debug( 'loaded html data! displaying..' )
 		self.CenterOnParent()
 		self.Raise()
 		self.Show()
-		self.box.OnLinkClicked = self.linkHandler
+		aboutWindow.instance = self
 
 	@staticmethod
 	def linkHandler(link):
 		wb.open(link.GetHref())
+
+	def OnClose( self, evt: wx.CloseEvent ):
+		self.Show(False)
