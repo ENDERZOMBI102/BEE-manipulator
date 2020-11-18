7c778dc3a74578342a3782aed3cf0dfdc846c517
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-11-18 16:14:29 +0100

    to finish auto release build

---
+++ upload_release.py
@@ -0,0 +1,25 @@
+from sys import argv
+
+from requests import post, get
+
+uploadUrl = get('https://api.github.com/repos/ENDERZOMBI102/BEE-manipulator/releases/latest') \
+	.json()['upload_url'] \
+	.replace('{?name,label}', '')
+
+
+files = {
+	'file': open('./BEEManipulator.7z', 'rb')
+}
+
+
+header = {
+	'Accept': 'application/vnd.github.v3+json',
+	'Content-Length': str( len( files['file'].read() ) ),
+	'Authorization': argv[1],  # github token
+	'Content-Type': 'application/octet-stream'
+}
+
+r = post(f'{uploadUrl}/?name=BEEManipulator.7z,label=BEE%20Manipulator', headers=header, files=files)
+
+print( r )
+print( r.json() )
