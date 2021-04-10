0e02045c852c641c91d16757f03a4e050261e9e7
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-04-11 00:30:38 +0200

    added settings added logging
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/cefManager.py
+++ src/cefManager.py
@@ -1,7 +1,11 @@
 import wx
 from cefpython3 import cefpython as cef
 
+import config
 import utilities
+from srctools.logger import get_logger
+
+LOGGER = get_logger()
 
 
 class CefManager:
@@ -9,11 +13,14 @@ class CefManager:
 	_timer: wx.Timer
 
 	def init( self ):
+		LOGGER.info('initializing cefPython!')
 		cef.Initialize(
-			dict(
+			settings=dict(
 				cache_path='',
+				product_version=config.version.__str__(),
 				user_agent='BEEManipulator',
 				persist_user_preferences=False,
+				downloads_enabled=True,
 				debug=utilities.devEnv,
 				log_file='./logs/cef.log' if utilities.frozen() else './../logs/cef.log',
 				context_menu={
@@ -22,7 +29,6 @@ class CefManager:
 					'navigation': utilities.devEnv,
 					'print': False,
 					'external_browser': False
-
 				}
 			)
 		)
@@ -31,6 +37,7 @@ class CefManager:
 		self._timer = wx.Timer()
 		self._timer.Notify = lambda: cef.MessageLoopWork()
 		self._timer.Start( 10 )  # 10ms timer
+		LOGGER.info('cefPython initialized!')
 
 	def stop( self ):
 		self._timer.Stop()
