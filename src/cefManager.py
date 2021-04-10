ae50ee3f1438545eb18117402aac274e476c7b07
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-04-10 23:26:44 +0200

    added browser setting to cef.Initialize() call
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/cefManager.py
+++ src/cefManager.py
@@ -1,13 +1,31 @@
 import wx
 from cefpython3 import cefpython as cef
 
+import utilities
+
 
 class CefManager:
 
 	_timer: wx.Timer
 
 	def init( self ):
-		cef.Initialize()
+		cef.Initialize(
+			dict(
+				cache_path='',
+				user_agent='BEEManipulator',
+				persist_user_preferences=False,
+				debug=utilities.devEnv,
+				log_file='./logs/cef.log' if utilities.frozen() else './../logs/cef.log',
+				context_menu={
+					'devtools': utilities.devEnv,
+					'view_source': utilities.devEnv,
+					'navigation': utilities.devEnv,
+					'print': False,
+					'external_browser': False
+
+				}
+			)
+		)
 		cef.DpiAware.EnableHighDpiSupport()
 
 		self._timer = wx.Timer()
