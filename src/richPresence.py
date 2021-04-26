14e8abe8d7587e45f5fc589537c0bc952d952c89
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-04-26 18:29:59 +0200

    made the rich presence work
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/richPresence.py
+++ src/richPresence.py
@@ -1,54 +1,110 @@
-import time
+from dataclasses import dataclass
+from time import time
 
-import pypresence
+import wx
+from pypresence import Presence, InvalidPipe
 
 import config
+from api.manager import Manager
 from srctools.logger import get_logger
 
 logger = get_logger()
 
 
-class richPresence:
-	rpc: pypresence.Presence = None
+@dataclass
+class RpcData:
+	# images
+	large_image: str
+	large_text: str
+	small_image: str
+	small_text: str
+	# text
+	state: str
+	details: str
+	start: float = time()
 
-	lastUpdate: dict = {
-		'start': int(time.time()),
-		'state': None,
-		'details': None,
-		'largeImage': None,
-		'largeText': None,
-		'smallImage': None,
-		'smallText': None
-	}
 
-	def __init__(self, token: str, oauth: str=None):
-		self.rpc = pypresence.Presence(token)
-		self.rpc.connect()
+class RichPresence(Manager):
 
-	def update(self, **kwargs):
-		# cycle in the args
-		for key, value in kwargs.items():
-			if key not in self.lastUpdate.keys():
-				continue  # update the value only if it exist in the lastUpdate dict
-			self.lastUpdate[key] = value
-		#
-		self.rpc.update(
-			start = self.lastUpdate['start'],
-			state = self.lastUpdate['state'],
-			details = self.lastUpdate['details'],
-			large_image = self.lastUpdate['largeImage'],
-			large_text = self.lastUpdate['largeText'],
-			small_image = self.lastUpdate['smallImage'],
-			small_text = self.lastUpdate['smallText']
+	lastData: RpcData = None
+	data: RpcData
+	presence: Presence
+	_timer: wx.Timer
+	_retryConnectTimer: wx.Timer
+	retryTime: int
+	wasConnected: bool = False
+
+	def init( self ) -> None:
+		self.presence = Presence( client_id=655075172767760384 )
+		self.retryTime = config.load('rpcReconnectTime')
+		# timers
+		self._timer = wx.Timer()
+		self._timer.Notify = lambda: self._update()
+		self._retryConnectTimer = wx.Timer()
+		self._retryConnectTimer.Notify = lambda: self.tryConnect()
+		# dummy data
+		self.data = RpcData(
+			# images
+			large_image=None,
+			large_text=None,
+			small_image=None,
+			small_text=None,
+			# text
+			state='dummy',
+			details='dummy'
 		)
+		# connect to discord
+		self.tryConnect()
 
-	def close(self):
-		self.rpc.close()
+	def stop( self ) -> None:
+		self._timer.Stop()
+		self._retryConnectTimer.Stop()
+		self.presence.close()
+
+	def update( self, **kwargs ):
+		for key, value in kwargs.items():
+			if key in self.data.__dict__:
+				setattr(self.data, key, value)
+			else:
+				logger.warning(f'Got unknown key for RichPresence.update()! key: {key}, value: {value}')
+
+	def _update( self ) -> None:
+		if self.data != self.lastData:
+			self.presence.update( **self.data.__dict__ )
+			self.lastData = RpcData( **self.data.__dict__ )
+
+	def tryConnect( self ) -> None:
+		try:
+			self.presence.connect()
+			self.wasConnected = True
+			self._timer.Start(1500)  # every 15 seconds, _update the presence data
+		except InvalidPipe:
+			logger.info(f'Cannot connect to discord RPC server, retrying in {self.retryTime}s')
+			self.wasConnected = False
+			self._retryConnectTimer.StartOnce( self.retryTime * 1000 )
+
+
+manager: RichPresence = RichPresence()
 
 
 if __name__ == '__main__':
-	r = richPresence(config.discordToken)
-	r.update()
+	from random import Random
 
-	while True:
-		eval(input('>'))
+	def update(evt):
+		manager.update(
+			state=Random().choice( ['State 1', 'State 2', 'State 3'] ),
+			details=Random().choice( [ 'Detail 1', 'Detail 2', 'Detail 3' ] )
+		)
+
+	app = wx.App()
+	manager.init()
+	win = wx.Frame(None)
+	btn = wx.Button(
+		parent=win,
+		label='Change presence'
+	)
+	btn.Bind(wx.EVT_BUTTON, update)
+	win.Show()
+	app.MainLoop()
+	manager.stop()
+
